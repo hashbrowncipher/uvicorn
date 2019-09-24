b7f95b99bee8a10ee8791625c03db99e42a548c9
Michael Chase <3686226+reallistic@users.noreply.github.com>
2019-09-24 16:13:48 -0700

    Add transport object to ASGI scope

--- uvicorn/protocols/http/h11_impl.py
+++ uvicorn/protocols/http/h11_impl.py
@@ -198,6 +198,7 @@ class H11Protocol(asyncio.Protocol):
                     "raw_path": raw_path,
                     "query_string": query_string,
                     "headers": self.headers,
+                    "transport": self.transport
                 }
 
                 should_upgrade = False
